97e6b6a2d3ed7740872c51246fbbdc421fea3141
Emanuele De Pellegrin <32999814+Cryoscopic-E@users.noreply.github.com>
2020-02-16 18:16:56 +0000

    Added log file listener

---
+++ PythonScripts/GameScripts/logListener.py
@@ -0,0 +1,18 @@
+import time
+import os
+
+#change to own documents folder location
+file = open("D:\\Documents\\The Witcher 3\\scriptslog.txt",'r')
+file_stat = os.stat("D:\\Documents\\The Witcher 3\\scriptslog.txt")
+file_size = file_stat[6]
+file.seek(file_size)
+
+while True:
+    position = file.tell()
+    line = file.readline()
+    if not line:
+        time.sleep(1)
+        file.seek(position)
+    else:
+        if line.startswith('[ChatMod]'):
+            print(line)
