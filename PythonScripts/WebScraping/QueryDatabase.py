6910f809335cd6829728a3f895813b462c540850
lorenzo456 <lorenzo456@users.noreply.github.com>
2020-02-16 21:23:04 +0000

    Merged files
    Added main files
    seperated connection to database
    editted speech to text
    added temporary language understanding

--- PythonScripts/WebScraping/QueryDatabase.py
+++ PythonScripts/WebScraping/QueryDatabase.py
@@ -1,34 +1,43 @@
-from pymongo import MongoClient
-import json
-
-try:
-        client = MongoClient('localhost', 27017)
-        print("FOUND")
-except:
-        print("FAILED")
-
-db = client.convAgentDB
-
-
+from ConnectToDatabase import *
 def GetEnemyInfo(enemyName, nameOfElement):
+    print("SYSTEM HAS FOUND KEYWORDS: " + enemyName + " AND " + nameOfElement)
     collection = db.Enemies
+    if enemyName == "griffin":
+        enemyName = "griffin (creature)"
+    print(enemyName)
     myquery = {"name": enemyName}
+    response = ""
     try:
         mydoc = collection.find(myquery)
         tempResult = str(mydoc[0][nameOfElement])
-        print(tempResult)
+        response = tempResult
     except:
-        print("Entry invalid")
+        print("ENEMY Entry invalid " + enemyName + "  "+ nameOfElement)
+        response = "Entry invalid"
+    return response
 
 def GetAlchemyInfo(itemName, nameOfElement):
+    print("SYSTEM HAS FOUND KEYWORDS: " + itemName + " AND " + nameOfElement)
     collection = db.Alchemy
     myquery = {"name": itemName}
+    response = ""
     try:
         mydoc = collection.find(myquery)
         tempResult = str(mydoc[0][nameOfElement])
-        print(tempResult)
+        response = tempResult
     except:
-        print("Entry invalid")
+      #  print("ALCHEMY Entry invalid " + itemName + "  "+ nameOfElement)
+        response = "Entry invalid"
+    return response
 
-GetEnemyInfo("Ghoul", "longCombatTactic")
-GetAlchemyInfo("Swallow", "ingredients")
+#
+# try:
+#         client = MongoClient('localhost', 27017)
+#         print("FOUND")
+# except:
+#         print("FAILED")
+#
+# db = client.convAgentDB
+#
+# #GetEnemyInfo("Ghoul", "longCombatTactic")
+# #GetAlchemyInfo("Swallow", "ingredients")
